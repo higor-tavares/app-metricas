265f4c0fea5c4b9ba52266faadafee12862ed53e
paulo.tavares <paulo.tavares@olxbr.com>
2022-07-16 18:49:30 -0300

    Adicionando um Histograma

--- src/main/java/com/higortavares/appmetricas/controller/HelloWorldController.java
+++ src/main/java/com/higortavares/appmetricas/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.higortavares.appmetricas.controller;
 
 import com.higortavares.appmetricas.metrics.MetricCounterService;
+import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,20 +15,30 @@ public class HelloWorldController {
   @Autowired private MetricCounterService metricCounterService;
 
   @GetMapping("/{success}")
-  public ResponseEntity<?> sayHello(@PathVariable("success") boolean success){
+  public ResponseEntity<?> sayHello(@PathVariable("success") boolean success) throws InterruptedException {
+    var now = Calendar.getInstance().getTimeInMillis();
+    Thread.sleep(Math.round(Math.random()*500));
     if(success) {
       metricCounterService.incrementEndpointStatus("say_hello", "200 OK");
+      var diff = ((Calendar.getInstance().getTimeInMillis()-now) /1000.0);
+      metricCounterService.responseTime(diff);
       return ResponseEntity.ok("Hello World!");
     }
     else {
       metricCounterService.incrementEndpointStatus("say_hello","500 - INTERNAL SERVER ERROR");
+      var diff = ((Calendar.getInstance().getTimeInMillis()-now) /1000.0);
+      metricCounterService.responseTime(diff);
       return ResponseEntity.internalServerError().build();
     }
   }
   @GetMapping("/usuariosLogados")
-  public ResponseEntity<?> loggedUsers() {
+  public ResponseEntity<?> loggedUsers() throws InterruptedException {
     var value = Math.random()*100;
+    var now = Calendar.getInstance().getTimeInMillis();
+    Thread.sleep(Math.round(Math.random()*500));
     metricCounterService.percentage("usuarios_logados_sucess", new AtomicReference<Double>(value));
+    var diff = ((Calendar.getInstance().getTimeInMillis()-now) /1000.0);
+    metricCounterService.responseTime(diff);
     return ResponseEntity.ok(value);
   }
 }
--- src/main/java/com/higortavares/appmetricas/metrics/MetricCounterService.java
+++ src/main/java/com/higortavares/appmetricas/metrics/MetricCounterService.java
@@ -2,14 +2,23 @@ package com.higortavares.appmetricas.metrics;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Histogram;
 import java.util.concurrent.atomic.AtomicReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MetricCounterService {
+  private static final String RESPONSE_TIME = "tempo_resposta_http";
+  private static final String RESPONSE_TIME_HELPER = "Tempo de resposta a uma requisição";
+  private static final double[] BUCKETS = {0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6};
   @Autowired
   private MeterRegistry meterRegistry;
+  @Autowired
+  private CollectorRegistry collectorRegistry;
+
+  private Histogram histogram;
 
   public void incrementEndpointStatus(String endpoint, String status){
     var counter = meterRegistry.counter(endpoint, "httpStatusResponse", status);
@@ -28,4 +37,10 @@ public class MetricCounterService {
     System.out.println(gauge.value());
   }
 
+  public void responseTime(Double value) {
+    if(this.histogram == null){
+      this.histogram = Histogram.build(RESPONSE_TIME, RESPONSE_TIME_HELPER).buckets(BUCKETS).register(collectorRegistry);
+    }
+    this.histogram.observe(value);
+  }
 }
